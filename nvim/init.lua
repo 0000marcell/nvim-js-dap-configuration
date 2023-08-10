96962fc1216870cac3e841e99996418390e252f5
____marcell <0000marcell@gmail.com>
2023-08-10 01:00:41 +0000

    updating the configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,18 @@
 local telescope_exists, telescope_builtin = pcall(require, "telescope.builtin")
 local treesitter_exists, treesitter_configs = pcall(require, "nvim-treesitter.configs")
 
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+
 if telescope_exists and treesitter_exists then
   -- ignore this start
   vim.g.mapleader = " "
@@ -19,7 +31,7 @@ if telescope_exists and treesitter_exists then
 
   treesitter_configs.setup {
     --ensure_installed = { "markdown", "javascript", "typescript", "ruby", "bash", "c", "lua", "vim", "vimdoc", "query" },
-    ensure_installed = { "ruby" },
+    ensure_installed = { "javascript" },
     sync_install = false,
     auto_install = true,
     highlight = {
@@ -55,6 +67,8 @@ return require('packer').startup(function(use)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+  use('tpope/vim-commentary')
+
   -- Debugging
   use {
     "mfussenegger/nvim-dap",
@@ -64,6 +78,7 @@ return require('packer').startup(function(use)
     wants = { 
       "nvim-dap-virtual-text", 
       "nvim-dap-ui", 
+      "nvim-dap-vscode-js",
       "telescope.nvim",
       "telescope-dap.nvim",
       "which-key.nvim",
@@ -71,121 +86,11 @@ return require('packer').startup(function(use)
     requires = {
       "theHamsta/nvim-dap-virtual-text",
       "rcarriga/nvim-dap-ui",
+      "mxsdev/nvim-dap-vscode-js",
       "nvim-telescope/telescope-dap.nvim",
     },
     config = function()
-
-	    require("dap").adapters["pwa-node"] = {
-		    type = "server",
-		    host = "localhost",
-		    port = "${port}",
-		    executable = {
-			    command = "node",
-			    args = {"/home/mmc/vscode-js-debug/js-debug/src/dapDebugServer.js",
-			    "${port}"},
-		    }
-	    }
-
-	    require("dap").configurations.javascript = {
-		    {
-			    type = "pwa-node",
-			    request = "launch",
-			    name = "Launch file",
-			    program = "${file}",
-			    cwd = "${workspaceFolder}",
-		    },
-	    }
-
-
-	    require("nvim-dap-virtual-text").setup {
-		    commented = true,
-	    }
-	    local dap, dapui = require "dap", require "dapui"
-	    dap.set_log_level('TRACE')
-
-	    dapui.setup {
-		    icons = {},
-		    layouts = {
-			    {
-				    elements = {
-					    {
-						    id = "watches",
-						    size = 1
-					    },
-				    },
-				    position = "bottom",
-				    size = 5
-			    },
-			    {
-				    elements = {
-					    {
-						    id = "scopes",
-						    size = 1
-					    },
-				    },
-				    position = "bottom",
-				    size = 5
-			    }
-		    },
-	    } -- use default
-	    dap.listeners.after.event_initialized["dapui_config"] = function()
-		    dapui.open()
-	    end
-	    dap.listeners.before.event_terminated["dapui_config"] = function()
-		    dapui.close()
-	    end
-	    dap.listeners.before.event_exited["dapui_config"] = function()
-		    dapui.close()
-	    end
-
-	    local whichkey = require "which-key"
-
-	    local keymap = {
-		    d = {
-			    name = "Debug",
-			    a = { "<cmd>lua require'dap'.restart()<cr>", "Restart" },
-			    c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-			    C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
-			    i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-			    o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-			    j = { "<cmd>lua require'dap'.up()<cr>", "Go up the stack frame" },
-			    k = { "<cmd>lua require'dap'.down()<cr>", "Go down the stack frame" },
-			    f = { "<cmd>Telescope dap frames<cr>", "Telescope frames" },
-			    b = { "<cmd>Telescope dap list_breakpoints<cr>", "Telescope list breakpoints" },
-			    r = { "<cmd>lua  require('dapui').float_element('repl', { width = 100, height = 20, enter = true, position = 'center' })<cr>", "Toggle Repl" },
-			    w = { "<cmd>lua  require('dapui').float_element('watches', { width = 100, height = 20, enter = true, position = 'bottom' })<cr>", "Toggle Watch" },
-			    v = { "<cmd>lua require'telescope'.extensions.dap.variables{}<cr>", "Toggle Repl" },
-			    s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
-			    t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-			    x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
-			    u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
-			    h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
-			    U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
-			    e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
-		    },
-	    }
-
-	    whichkey.register(keymap, {
-		    mode = "n",
-		    prefix = "<leader>",
-		    buffer = nil,
-		    silent = true,
-		    noremap = true,
-		    nowait = false,
-	    })
-
-	    local keymap_v = {
-		    name = "Debug",
-		    e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
-	    }
-	    whichkey.register(keymap_v, {
-		    mode = "v",
-		    prefix = "<leader>",
-		    buffer = nil,
-		    silent = true,
-		    noremap = true,
-		    nowait = false,
-	    })
+        require("dapconfig").setup()
     end,
   }
 end)
